d49d688e725a6b691981f06de074ab80c6300b79
goutam-goyal <goutamgoyal31@gmail.com>
2021-10-09 00:23:31 +0530

    select an organ updated

--- src/components/Donate/donate.js
+++ src/components/Donate/donate.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { getCurrentUser } from "../../utils/auth";
+// import { getCurrentUser } from "../../utils/auth";
 import TextField from "@mui/material/TextField";
 import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
@@ -7,9 +7,9 @@ import FormControl from "@mui/material/FormControl";
 import "./donate.css";
 import FileUploadOutlinedIcon from "@mui/icons-material/FileUploadOutlined";
 import Button from "@mui/material/Button";
-import InputLabel from '@mui/material/InputLabel';
+import InputLabel from "@mui/material/InputLabel";
 const DonatePage = () => {
-  const { name } = getCurrentUser();
+  // const { name } = getCurrentUser();
 
   return (
     <div class="container">
@@ -27,23 +27,44 @@ const DonatePage = () => {
             />
           </div>
           <div class="form-group flex ">
-            <TextField className="donor-age" id="donor-age" label="Donor Age" variant="filled" />
-            <TextField className="blood-group" id="blood-group" label="Blood Group" variant="filled" />
-          </div>
-          <div class="form-group ">
-            <InputLabel  id="organ-select">Select an organ</InputLabel>
-            <Select
-              labelId="organ-select"
-              id="organs"
-              className="full-width"
+            <TextField
+              className="donor-age"
+              id="donor-age"
+              label="Donor Age"
               variant="filled"
-            >
-              <MenuItem value="kidney">Kidney</MenuItem>
-              <MenuItem value="liver">Liver</MenuItem>
-              <MenuItem value="heart">Heart</MenuItem>
-              <MenuItem value="eyes">Eyes</MenuItem>
-            </Select>
+            />
+            <TextField
+              className="blood-group"
+              id="blood-group"
+              label="Blood Group"
+              variant="filled"
+            />
           </div>
+          <FormControl>
+              <InputLabel id="organ-select">Select an organ</InputLabel>
+            <div class="form-group ">
+              <Select
+                labelId="organ-select"
+                id="organs"
+                className="full-width"
+                variant="filled"
+              >  
+                <MenuItem value="liver">Liver</MenuItem>
+                <MenuItem value="kidney">Kidney</MenuItem>
+                <MenuItem value="pancreas">Pancreas</MenuItem>
+                <MenuItem value="heart">Heart</MenuItem>
+                <MenuItem value="lung">Lung</MenuItem>
+                <MenuItem value="intestine">Intestine</MenuItem>
+                <MenuItem value="corneas">Corneas</MenuItem>
+                <MenuItem value="middle-ear">Midddle Ear</MenuItem>
+                <MenuItem value="skin">Skin</MenuItem>
+                <MenuItem value="bone">Bone</MenuItem>
+                <MenuItem value="bone-marrow">Bone marrow</MenuItem>
+                <MenuItem value="heart-valves">Heart Valves</MenuItem>
+                {/* <MenuItem value=""><em>None</em> </MenuItem> */}
+              </Select>
+            </div>
+          </FormControl>
           <div class="form-group">
             <TextField
               className="full-width"
@@ -53,12 +74,21 @@ const DonatePage = () => {
             />
           </div>
           <div class="form-group">
-            <Button className="upload-container" variant="outlined" component="span" startIcon={<FileUploadOutlinedIcon />}>
+            <Button
+              className="upload-container"
+              variant="outlined"
+              component="span"
+              startIcon={<FileUploadOutlinedIcon />}
+            >
               Upload Prescription
             </Button>
           </div>
           <div class="form-group">
-            <Button variant="filled" component="span">
+            <Button
+              className="upload-container"
+              variant="outlined"
+              component="span"
+            >
               Next
             </Button>
           </div>
